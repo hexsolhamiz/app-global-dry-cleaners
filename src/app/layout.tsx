a16e399d6a545c4be11c5ca519c969329b3d5ff1
hexsolhamiz <hexsolhamiz@gmail.com>
2026-02-10 23:00:30 +0500

    minor-fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
-import { StripeProvider } from "@/providers/StripeProvider";
 
 const poppins = Poppins({
   variable: "--font-poppins",
